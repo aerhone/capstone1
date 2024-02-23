b74908aac9441c60ccf8bb8d4706cb95dd2abd68
markaerhonepaul.palma <markaerhonepaul.palma@gmail.com>
2024-02-23 20:11:21 +0800

    fix loading screen

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ function initializeTypingEffect() {
 setTimeout(initializeTypingEffect, 1500);
 
 
-let loader = document.querySelector("#preloader")
-window.addEventListener("load", function(){
-    loader.style.display = "none"
-})
+document.addEventListener("DOMContentLoaded", function() {
+    // Continuously check for the document readiness
+    var checkReadyState = setInterval(function() {
+        if (document.readyState === "complete") {
+            // If the document is fully loaded, clear the interval and hide the preloader
+            clearInterval(checkReadyState);
+            var preloader = document.getElementById("preloader");
+            preloader.style.display = "none";
+
+            // Add any additional initialization code here that you want to run after the document is fully loaded
+        } else {
+            // If the document is not fully loaded, display the preloader
+            var preloader = document.getElementById("preloader");
+            preloader.style.display = "block";
+        }
+    }, 100); // Check every 100 milliseconds (adjust as needed)
+});
